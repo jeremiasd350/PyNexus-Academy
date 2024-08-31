bdffb607e96370abbe9e59ab4967656bf6b35cf1
jeremiasd350 <jeremiasduarterosa@gmail.com>
2024-08-31 09:28:48 -0400

    cambios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const divMenu = document.getElementById("div-menu");
 const divClose = document.getElementById("div-close");
-const navResponsive = document.getElementById("nav-responsive");
+const navResponsive = document.getElementById("nav-responsive-out");
 const navRegistrar = document.getElementById("nav-registrar");
 
 navResponsive.style.display = "none";
